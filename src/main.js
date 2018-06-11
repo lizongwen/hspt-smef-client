c5c77e1c33d730a127ff9a10ecce62088c6cece5
lizongwen <93700983@qq.com>
2018-06-11 16:01:21 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import './style/base.css';
 import './style/icon/iconfont.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
-import router from './router'
+import router from './router';
+import axios from 'axios';
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 require('./mock.js');
 
 /* eslint-disable no-new */
