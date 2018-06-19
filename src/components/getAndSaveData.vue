a101cb475fc6476f42c0ebe6b0b9f8fb6a551367
advancingKoala <milaiduoduo4567@163.com>
2018-06-19 17:36:16 +0800

    new，新增工商信息页面 new，新增getAndSaveData.vue组件

---
+++ src/components/getAndSaveData.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrap clearfix">
+    <span class="title">{{title}}</span>
+    <el-button class="save" type="primary" size="medium">保存</el-button>
+    <el-button class="getData" size="medium">获取数据</el-button>
+  </div>
+</template>
+<script type='text/ecmascript-6'>
+  export default{
+    data(){
+      return {}
+    },
+    props: ['title']
+  }
+</script>
+<style lang='scss' rel="stylesheet/scss">
+  @import "@/style/commonStyle.scss";
+  .wrap {
+    padding: 19px;
+    background: #FFFFFF;
+    box-shadow: 0 1px 4px 0 rgba(0,0,0,0.10);
+    //border-bottom: 1px solid $border-color-light;
+    .title {
+      font-family: PingFangSC-Regular;
+      font-size: 16px;
+      color: #000000;
+      line-height: 36px;
+    }
+    .getData, .save {
+      float: right;
+    }
+    .getData{
+      margin-right:10px;
+    }
+  }
+</style>
--- src/page/company/base/gsxx.vue
+++ src/page/company/base/gsxx.vue
@@ -1,15 +1,161 @@
 <template>
-	<div>
-    <h1>我是基本信息页面</h1>
+  <div>
+    <v-getAndSaveData title="工商基本信息"></v-getAndSaveData>
+    <el-form class="form clearfix" size="small" :label-position="labelPosition" label-width="100px" :model="formInline">
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="注册日期:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="注册机关:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="社会信用代码:">
+            <el-input :disabled="true" v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="组织机构代码:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="注册号:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="注册资本:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="企业类型:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="成立年份:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="营业期限:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="注册地址:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="法人代表:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="电话:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="传真:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="网站:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="公司人数:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :offset="1" :span="10">
+          <el-form-item label="销售收入:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="0">
+        <el-col :offset="1" :span="10">
+          <el-form-item label="净资产:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="1" :span="21">
+          <el-form-item label="经营范围:">
+            <el-input v-model="formInline.user"></el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  import getAndSaveData from '@/components/getAndSaveData.vue';
+  export default {
+    components: {
+      'v-getAndSaveData': getAndSaveData
+    },
+    data() {
+      return {
+        labelPosition: 'right',
+        formInline: {
+          user: '',
+          region: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
 </script>
 
-<style>
+<style scoped lang="scss">
+  @import "@/style/commonStyle.scss";
+
+  .form {
+    margin-top: 4px;
+    padding: 43px 20px;
+    background: $background-color-inside;
+  }
 
 </style>
+
+
+
+
+
+
+
+
+
