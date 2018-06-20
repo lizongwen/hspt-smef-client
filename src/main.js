723b63823409315da96abc3027a078aa9546f13b
lizongwen <93700983@qq.com>
2018-06-20 21:32:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -10,13 +10,14 @@ import './style/common.css';
 import App from './App';
 import router from './router';
 import axios from 'axios';
+import Qs from 'qs';
 
 // import './style/commonStyle.scss'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
-require('./mock.js');
+// require('./mock.js');
 
 /* eslint-disable no-new */
 new Vue({
