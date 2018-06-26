3c049255886a4034c00b2eb4c3ddcd8e0a818e98
advancingKoala <milaiduoduo4567@163.com>
2018-06-26 18:26:25 +0800

    单击表格控件可编辑之半成版本（将不再使用）

--- src/main.js
+++ src/main.js
@@ -18,62 +18,16 @@ import api from './utils/api';
 require('./mock.js');
 
 // import './style/commonStyle.scss'
+// 使用vue-easytable组件
 import 'vue-easytable/libs/themes-base/index.css'
-// 导入 table 和 分页组件
-import {VTable, VPagination} from 'vue-easytable'
-import tableOperation from './components/globalComponent'
-// 注册到全局
+import {VTable} from 'vue-easytable'
 Vue.component(VTable.name, VTable)
-Vue.component(VPagination.name, VPagination)
-Vue.use(tableOperation);
 
-// Vue.component('v-table-operation', {
-//   template: ` <div>
-//     <span v-if="!editingFlag">
-//       <a href="" @click.prevent="update(rowData,index)">编辑</a>
-//       <a href="" @click.stop.prevent="deleteRow(rowData,index)">删除</a>
-//     </span>
-//     <span v-if="editingFlag">
-//     <a href="" @click.stop.prevent="save(rowData,index)">保存</a>
-//     <a href="" @click.stop.prevent="cancel(rowData,index)">取消</a>
-//     </span>
-//   </div>`,
-//   props: {
-//     rowData: {
-//       type: Object
-//     },
-//     field: {
-//       type: String
-//     },
-//     index: {
-//       type: Number
-//     }
-//   },
-//   computed: {
-//     editingFlag(){
-//       return this.$store.state.company.editingState;
-//     }
-//   },
-//   methods: {
-//     update(){
-//       let params = {type: 'edit', index: this.index, rowData: this.rowData};
-//       this.$emit('on-custom-comp', params);
-//     },
-//     deleteRow(){
-//       let params = {type: 'delete', index: this.index};
-//       this.$emit('on-custom-comp', params);
-//     },
-//     save(){
-//       let params = {type: 'save', index: this.index, rowData: this.rowData};
-//       this.$emit('on-custom-comp', params);
-//     },
-//     cancel(){
-//       let params = {type: 'cancel', index: this.index, rowData: this.rowData};
-//       this.$emit('on-custom-comp', params);
-//     }
-//   }
-// })
-//--end----------vue-easytable所使用的组件
+// import tableOperation from './components/globalComponent'
+
+//全局注册table操作列组件
+// Vue.use(tableOperation);
+
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
